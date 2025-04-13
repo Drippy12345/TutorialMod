ff453465465d05439e97ec90c80527910fba9879
Goober290 <jennie.breitrick@gmail.com>
2025-04-13 18:01:23 -0500

    Important Changes Made!

--- src/main/java/net/karl/tutorialmod/tutorialmod.java
+++ src/main/java/net/karl/tutorialmod/tutorialmod.java
@@ -16,10 +16,8 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(tutorialmod.MOD_ID)
 public class tutorialmod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public tutorialmod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
